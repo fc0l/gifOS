6235f0f14b9e5e5a90561b6d853f1d75bd1c6179
fc0l <fc0l@pop-os.localdomain>
2020-11-06 14:00:31 -0300

    html structure

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-// connect with trending API
-const trending_text = document.getElementById("trending");
-const trending = fetch('https://api.giphy.com/v1/gifs/trending?api_key=2HgAvudoWdCgxf4i72FF9xf1m4N2VBib&limit=25&rating=g');
-trending.then(resp => resp.json())
-.then(json => {
-    for(let i = 0;i < 3; i++){
-        trending_text.innerHTML += `<a href="${json.data[i].url}" class="trend-a">${json.data[i].title}, </a>`;
-    }
-});
+// // connect with trending API
+// const trending_text = document.getElementById("trending");
+// const trending = fetch('https://api.giphy.com/v1/gifs/trending?api_key=2HgAvudoWdCgxf4i72FF9xf1m4N2VBib&limit=25&rating=g');
+// trending.then(resp => resp.json())
+// .then(json => {
+//     for(let i = 0;i < 3; i++){
+//         trending_text.innerHTML += `<a href="${json.data[i].url}" class="trend-a">${json.data[i].title}, </a>`;
+//     }
+// });
 
-// search input key up event
-const search_box = document.getElementById("search");
-const search_results = document.getElementById("search-results");
-let resultados;
-let p = document.createElement("P");
-let search_field;
+// // search input key up event
+// const search_box = document.getElementById("search");
+// const search_results = document.getElementById("search-results");
+// let resultados;
+// let p = document.createElement("P");
+// let search_field;
 
-search_box.addEventListener("keyup", function (e) {
-    search_field = search_box.value;
-    if(search_box.value != ""){
-    fetch(`https://api.giphy.com/v1/tags/related/${search_field}?api_key=2HgAvudoWdCgxf4i72FF9xf1m4N2VBib&`)
-    .then(resp => resp.json())
-    .then(json => {
-        resultados = json.data;
-        search_results.innerHTML = "";
-        for(let i = 0;i<resultados.length; i++){
-            console.log(resultados[i].name);
-            search_results.innerHTML = `<p>${resultados[i].name}</p>`;
-        }
-})    
-}})
+// search_box.addEventListener("keyup", function (e) {
+//     search_field = search_box.value;
+//     if(search_box.value != ""){
+//     fetch(`https://api.giphy.com/v1/tags/related/${search_field}?api_key=2HgAvudoWdCgxf4i72FF9xf1m4N2VBib&`)
+//     .then(resp => resp.json())
+//     .then(json => {
+//         resultados = json.data;
+//         search_results.innerHTML = "";
+//         for(let i = 0;i<resultados.length; i++){
+//             console.log(resultados[i].name);
+//             search_results.innerHTML = `<p>${resultados[i].name}</p>`;
+//         }
+// })    
+// }})
 
 
 
